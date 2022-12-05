2394d48a697e928e6a6d9ca2f272425256a89e8c
jaster72 <118030382+jaster72@users.noreply.github.com>
2022-12-05 10:20:37 -0500

    Update processing.py

--- processing.py
+++ processing.py
@@ -1,2 +1,14 @@
-def do_calculation(number1, number2):
-    return number1 + number2
+import os
+import openai
+openai.api_key = "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"
+
+# customPrompt = input("What do you want to ask the oracle? ")
+
+def run_GPT(gptPrompt):
+    response_API = openai.Completion.create(
+    model="text-davinci-003",
+    prompt=gptPrompt,
+    max_tokens=1000,
+    temperature=1.1
+    )
+    return response_API.choices[0].text
